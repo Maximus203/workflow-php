c8d316575068a76c49b05851b2a23f046037f24d
Maximus203 <printf0cherif@gmail.com>
2022-09-12 20:10:09 +0000

    code-008

--- site.php
+++ site.php
@@ -9,25 +9,22 @@
 </head>
 
 <body>
-
-    <form action="site.php" method="post">
-        Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
-        Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
-        Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
-
-        <input type="submit">
-    </form>
-
-
     <?php
-    if (isset($_POST['fruits'])) {
-        $fruits = $_POST['fruits'];
-        echo $fruits[0];
-        echo $fruits[1];
-        echo $fruits[2];
-    }
-    ?>
+    /*
+        <form action="site.php" method="post">
+            Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
+            Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
+            Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
+    
+            <input type="submit">
+        </form>
+    */
 
+    $grades = array("Jim" => "A+", "Pam" => "A+", "Oscar" => "B+", "Moussa" => "C+");
+    echo $grades["Jim"]
+
+
+    ?>
 </body>
 
 </html>
