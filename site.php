457e8a9a25eebf41c0a22395f895625afb921952
Maximus203 <printf0cherif@gmail.com>
2022-09-12 14:30:10 +0000

    code-001

--- site.php
+++ site.php
@@ -10,9 +10,28 @@
 
 <body>
     <?php
+    $characterName = "Cherif DIOUF";
+    $characterAge = 23;
+
     echo "<h1>Site de Thera</h1>";
     echo "<hr>";
-    echo "<p>This is my website</p>"
+
+
+    echo "<p>This is my website</p>";
+    echo "<p>My name is: <b> $characterName </b></p>";
+
+    $phrase = "Code is the process to create bugs";
+    $after = strtolower($phrase);
+
+    echo "<p>The sentences :$phrase give in lower case: $after</p>";
+
+    $after = strtoupper($phrase);
+    echo "<p>And in uppercase: $after</p>";
+
+    echo strlen($phrase);
+
+
+
     ?>
 </body>
 
