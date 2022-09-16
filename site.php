766105309042f05dca487eaa9471310ee05e17fb
Maximus203 <printf0cherif@gmail.com>
2022-09-16 15:44:22 +0000

    code-018

--- site.php
+++ site.php
@@ -25,21 +25,20 @@
         var $title;
         var $author;
         var $pages;
+
+        function __construct($aTitle, $aAuthor, $aPages)
+        {
+            $this->title = $aTitle;
+            $this->author = $aAuthor;
+            $this->pages = $aPages;
+        }
     }
 
-    $book1 = new Book;
-    $book2 = new Book;
+    $book1 = new Book("Harry Potter", "JK Rowling", 323);
+    $book2 = new Book("Lord of the rings", "Tolkien", 277);
+    $book2->title = "Hunger Games";
 
-    $book1->title = "Harry Potter";
-    $book1->author = "JK Rowling";
-    $book1->pages = 323;
-
-    $book2->title = "Lord of the rings";
-    $book2->author = "Tolkien";
-    $book2->pages = 277;
-
-    echo $book1->author;
-    echo $book2->pages;
+    echo $book2->title;
 
     ?>
 </body>
